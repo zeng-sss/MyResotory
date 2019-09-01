b018a2c379d130d1ae037eb786004d1a4093489a
zeng <2936642799@qq.com>
2019-09-01 11:36:07 +0800

    初始化

---
+++ itheima_spring_test/src/main/java/com/itheima/mapper/RoleDao.java
@@ -0,0 +1,13 @@
+package com.itheima.mapper;
+
+import com.itheima.domain.Role;
+
+import java.util.List;
+
+public interface RoleDao {
+    List<Role> findAll();
+
+    void save(Role role);
+
+    List<Role> findRoleByUserId(Long id);
+}
